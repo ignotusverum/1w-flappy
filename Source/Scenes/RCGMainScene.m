a01f0878d7281ae379212772b3ef6f96a38d7eb3
Vlad Z <vladislavz1991@gmail.com>
2015-10-10 13:28:17 -0400

    infinite ground logic

--- Source/Scenes/RCGMainScene.m
+++ Source/Scenes/RCGMainScene.m
@@ -13,15 +13,41 @@
 @property (nonatomic, weak) CCSprite * heroSprite;
 @property (nonatomic, weak) CCPhysicsNode * mainPhysicsNode;
 
+@property (nonatomic, weak) CCNode * ground1Node;
+@property (nonatomic, weak) CCNode * ground2Node;
+@property (nonatomic, strong) NSArray * arrayOfGroundNodes;
+
 @end
 
 @implementation RCGMainScene
+
+
+- (void) didLoadFromCCB
+{
+    self.arrayOfGroundNodes = @[self.ground1Node, self.ground2Node];
+}
 
 
 - (void) update:(CCTime)delta
 {
     self.heroSprite.position = ccp(self.heroSprite.position.x + delta * RCGScrollSpeed, self.heroSprite.position.y);
     self.mainPhysicsNode.position = ccp(self.mainPhysicsNode.position.x - delta * RCGScrollSpeed, self.mainPhysicsNode.position.y);
+    
+    [self updateGroundUI];
+}
+
+
+- (void) updateGroundUI
+{
+    for (CCNode * groundNode in self.arrayOfGroundNodes) {
+        CGPoint groundWorldPosition = [self.mainPhysicsNode convertToWorldSpace:groundNode.position];
+        
+        CGPoint groundScreenPosition = [self convertToNodeSpace:groundWorldPosition];
+        
+        if (groundScreenPosition.x <= (-1 * groundNode.contentSize.width)) {
+            groundNode.position = ccp(groundNode.position.x + 2 * groundNode.contentSize.width, groundNode.position.y);
+        }
+    }
 }
 
 
