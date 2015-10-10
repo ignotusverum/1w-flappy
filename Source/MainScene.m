0330b30b4b8f170599ad11e5b0b0d87bf5f9e054
Vlad Z <vladislavz1991@gmail.com>
2015-10-10 13:07:36 -0400

    update proj filesystem

--- Source/MainScene.m
+++
@@ -1,9 +0,0 @@
-#import "MainScene.h"
-
-
-
-@implementation MainScene
-
-
-
-@end
---
+++ Source/RCGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RCGAppDelegate.h
+//  Flappy
+//
+//  Created by Vlad Zagorodnyuk on 10/10/15.
+//  Copyright © 2015 Apportable. All rights reserved.
+//
+
+#import "CCAppDelegate.h"
+
+#import "cocos2d.h"
+
+@interface RCGAppDelegate : CCAppDelegate
+
+@end
